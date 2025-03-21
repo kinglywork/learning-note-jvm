78c62ac9ee04d19f0fce6d18730acf254b4c16bd
Liu Yi <kinglywork@gmail.com>
2025-03-21 16:17:40 +0800

    add 13-3

--- code-samples/13-2-vector.java
+++ code-samples/13-2-vector.java
@@ -1,35 +1,39 @@
 // although the get, add, remove, size is thread safe (with sychronized block), it's not thread safe in below code,
 // as the code including size and remove or code including size and get are not atomic, concurrent running will lead to unexpected result.
 // need to use sychronized block around the loop code with size and remove and size and get to make sure thread safe.
-private static Vector<Integer> vector = new Vector<Integer>();
 
-public static void main(String[] args) {
-    while (true) {
-        for (int i = 0; i < 10; i++) {
-            vector.add(i);
+public class VectorExample {
+    private static Vector<Integer> vector = new Vector<Integer>();
+
+    public static void main(String[] args) {
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                vector.add(i);
+            }
+
+            Thread removeThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        vector.remove(i);
+                    }
+                }
+            });
+
+            Thread printThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        System.out.println(vector.get(i));
+                    }
+                }
+            });
+
+            removeThread.start();
+            printThread.start();
+
+            while (Thread.activeCount() > 20)
+                ;
         }
-
-        Thread removeThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < vector.size(); i++) {
-                    vector.remove(i);
-                }
-            }
-        });
-
-        Thread printThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < vector.size(); i++) {
-                    System.out.println(vector.get(i));
-                }
-            }
-        });
-
-        removeThread.start();
-        printThread.start();
-
-        while (Thread.activeCount() > 20);
     }
 }
